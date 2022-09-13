6e7784506f711920540b8d1e187b6deb074ffb47
brookmeko <brookmeko42@gmail.com>
2022-09-13 02:14:04 -0700

    print

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,13 @@ int main(void)
 int c;
 for (c = 48; c <= 57; c++)
 {
-	putchar(c);				
-	if (c != 57)
-	{							
-		putchar(',');					
-		putchar(' ');
-	}
+putchar(c);				
+if (c != 57)
+{							
+	putchar(',');					
+	putchar(' ');
 }
-	putchar('\n');
-	return (0);
+}
+putchar('\n');
+return (0);
 }
