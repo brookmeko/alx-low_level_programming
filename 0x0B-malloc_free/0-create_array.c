1ac54cadaffda3559d7f602b535d8bfa999052ab
brookmeko <brookmeko42@gmail.com>
2022-10-19 02:32:25 -0700

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,15 +1,28 @@
-#include "bootcamp.h"
-/**
- * malloc_checked - allocates memory using malloc
- * @b: unsigned int of amount of needed memory
- * Return: void
- */
-void *malloc_checked(unsigned int b)
-{
-	void *p;
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-	p = malloc(b);
-	if (p == NULL)
-		exit(98);
-	return (p);
+/**
+ * create_array - Entry point
+ *@size: size of the array
+ *@c: character
+ * Return: a pointer to the array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array = NULL;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	if (size != 0)
+	{
+		array = (char *)malloc(size * sizeof(char));
+		if (array != NULL)
+		{
+			for (i = 0; i < size; i++)
+				array[i] = c;
+		}
+	}
+	return (array);
 }
